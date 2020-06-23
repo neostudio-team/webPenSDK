73eac47a9969db1cae434102a2673a24e646220a
AramMoon <arammoon11@gmail.com>
2020-06-23 15:30:36 +0900

    delete ts file

--- PenSDK/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import PenController from './PenCotroller/PenController'
-import Dot from './API/Dot'
-import PenMessageType, {SettingType} from './API/PenMessageType'
-
-// export default PenController
-export {PenController, Dot, PenMessageType, SettingType}
