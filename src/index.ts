7fc56c96bfe8808c47045537d3f3f227bd6f4b07
minihallo-neo <loitermin@neolab.net>
2022-04-27 11:41:41 +0900

    v0.0.17
    - PenHelper를 sdk로 이동, typescript로 migration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import PenController from './PenCotroller/PenController'
+import PenHelper from './PenCotroller/PenHelper'
 import Dot from './API/Dot'
 import PenMessageType, {SettingType} from './API/PenMessageType'
 
 // export default PenController
-export {PenController, Dot, PenMessageType, SettingType}
+export {PenController, Dot, PenHelper, PenMessageType, SettingType}
